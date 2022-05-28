33bf335f646d2c65121517dafcf39681a4760389
Inan Hira <inan@tilde.team>
2022-05-28 17:16:55 +0100

    Clean up JS syntax
    As per the recommendations from `npm run test`, I updated the code and
    cleaned up a bit for better readability.

--- src/createManifest.js
+++ src/createManifest.js
@@ -1,78 +1,82 @@
-const JSZip = require('jszip')
-const fileSaver = require('file-saver')
-const createFiles = require('./fileCreater')
+const JSZip = require('jszip');
+const fileSaver = require('file-saver');
+const createFiles = require('./fileCreater');
 
 const createManifest = response => {
-  const masterZip = new JSZip()
+  const masterZip = new JSZip();
   const manifestJson = {
-    'manifest_version': 2,
-    'name': response.name.trim() || 'My extension',
-    'description': response.description || 'description',
-    'version': response.version || '1.1.1',
-    'icons': {
-      '64': 'icons/icon.png'
-    }
-  }
-  const zip = masterZip.folder(manifestJson.name)
+    manifest_version: 2,
+    name: response.name.trim() || 'My extension',
+    description: response.description || 'description',
+    version: response.version || '1.1.1',
+    icons: {
+      64: 'icons/icon.png',
+    },
+  };
+  const zip = masterZip.folder(manifestJson.name);
 
-  const createorPromises = []
-  createorPromises.push(createFiles.icon(zip))
+  const createorPromises = [];
+  createorPromises.push(createFiles.icon(zip));
 
   if (response.background_script) {
     manifestJson.background = {
-      'scripts': ['background_script.js']
-    }
-    createorPromises.push(createFiles.bg(zip))
+      scripts: ['background_script.js'],
+    };
+    createorPromises.push(createFiles.bg(zip));
   }
 
   if (response.content_script) {
     manifestJson.content_scripts = [
       {
-        'matches': ['*://*.mozilla.org/*'],
-        'js': ['content_script.js']
-      }
-    ]
-    createorPromises.push(createFiles.cs(zip))
+        matches: [
+          '*://*.mozilla.org/*',
+        ],
+        js: [
+          'content_script.js',
+        ],
+      },
+    ];
+    createorPromises.push(createFiles.cs(zip));
   }
 
   if (response.browser_action) {
     manifestJson.browser_action = {
-      'default_icon': {
-        '64': 'icons/icon.png'
+      default_icon: {
+        64: 'icons/icon.png',
       },
-      'default_popup': 'browserAction/index.html',
-      'default_title': manifestJson.name
-    }
-    createorPromises.push(createFiles.popup(zip, 'browserAction'))
+      default_popup: 'browserAction/index.html',
+      default_title: manifestJson.name,
+    };
+    createorPromises.push(createFiles.popup(zip, 'browserAction'));
   }
 
   if (response.page_action) {
     manifestJson.page_action = {
-      'default_icon': {
-        '64': 'icons/icon.png'
+      default_icon: {
+        64: 'icons/icon.png',
       },
-      'default_popup': 'pageAction/index.html',
-      'default_title': manifestJson.name
-    }
-    createorPromises.push(createFiles.popup(zip, 'pageAction'))
+      default_popup: 'pageAction/index.html',
+      default_title: manifestJson.name,
+    };
+    createorPromises.push(createFiles.popup(zip, 'pageAction'));
   }
 
   if (response.options_ui) {
     manifestJson.options_ui = {
-      'page': 'options/index.html'
-    }
-    createorPromises.push(createFiles.popup(zip, 'options'))
+      page: 'options/index.html',
+    };
+    createorPromises.push(createFiles.popup(zip, 'options'));
   }
 
   return Promise.all(createorPromises).then(data => {
-    zip.file('manifest.json', JSON.stringify(manifestJson, null, 2))
+    zip.file('manifest.json', JSON.stringify(manifestJson, null, 2));
     return masterZip
       .generateAsync({ type: 'blob' })
       .then(content => {
         const name = manifestJson.name.replace(/[`~!@#$ %^&*()_|+\-=÷¿?;:'",.<>{}[\]\\/]/gi, '-').toLowerCase()
         return fileSaver.saveAs(content, `${name}.zip`)
-      })
-  }).catch(e => false)
+      });
+  }).catch(e => false);
 }
 
-module.exports = createManifest
+module.exports = createManifest;
--- src/script.js
+++ src/script.js
@@ -1,35 +1,39 @@
-const createManifest = require('./createManifest')
-const response = {}
+const createManifest = require('./createManifest');
+const response = {};
 
 const nextQuestion = id => {
   Array
     .from(document.getElementsByClassName('question'))
-    .forEach(e => e.setAttribute('data-active', 'false'))
+    .forEach(e => e.setAttribute('data-active', 'false'));
 
-  document.getElementById(`q-${id}`).setAttribute('data-active', 'true')
+  document.getElementById(`q-${id}`).setAttribute('data-active', 'true');
 
   setTimeout(() => {
-    if (document.querySelector(`#q-${id} input[type="text"]`)) document.querySelector(`#q-${id} input[type="text"]`).focus()
-    if (document.querySelector(`#q-${id} .button`)) document.querySelector(`#q-${id} .button`).focus()
-  }, 10)
-}
+    if (document.querySelector(`#q-${id} input[type="text"]`)) {
+      document.querySelector(`#q-${id} input[type="text"]`).focus();
+    }
+    if (document.querySelector(`#q-${id} .button`)) {
+      document.querySelector(`#q-${id} .button`).focus();
+    }
+  }, 10);
+};
 
 const validate = (question, value) => {
   if (question === 'version') {
-    return (value.match(/^(\d+\.)?(\d+\.)?(\*|\d+)$/i))
+    return (value.match(/^(\d+\.)?(\d+\.)?(\*|\d+)$/i));
   }
-}
+};
 
 const validateAndSetValue = e => {
-  const question = e.getAttribute('data-question')
-  const isValidate = e.getAttribute('data-validate')
-  const value = e.value || ''
+  const question = e.getAttribute('data-question');
+  const isValidate = e.getAttribute('data-validate');
+  const value = e.value || '';
   if (!isValidate || validate(question, value)) {
-    response[question] = value
-    return true
+    response[question] = value;
+    return true;
   }
-  return false
-}
+  return false;
+};
 
 // actions for text inputs
 Array
@@ -38,9 +42,13 @@ Array
     e => e.addEventListener('click', ev => {
       Array
         .from(document.getElementsByClassName('error'))
-        .forEach(e => { e.style.display = 'none' })
-      if (validateAndSetValue(ev.target.previousElementSibling)) nextQuestion(ev.target.getAttribute('data-next'))
-      else ev.target.nextElementSibling.style.display = 'block'
+        .forEach(e => { e.style.display = 'none' });
+      if (validateAndSetValue(ev.target.previousElementSibling)) {
+        nextQuestion(ev.target.getAttribute('data-next'));
+      }
+      else {
+        ev.target.nextElementSibling.style.display = 'block';
+      }
     })
   )
 
@@ -48,26 +56,30 @@ Array
   .from(document.querySelectorAll('input[type="text"]'))
   .forEach(
     e => e.addEventListener('keyup', ev => {
-      if (ev.keyCode === 13) e.nextElementSibling.click()
+      if (ev.keyCode === 13) {
+        e.nextElementSibling.click();
+      }
     })
-  )
+  );
 
 // actions for button inputs
 window.setValue = (question, answer, next) => {
-  response[question] = answer
+  response[question] = answer;
   if (next === 0) {
     createManifest(response)
-      .then(e => window.changeScreen('thanks'))
-    return true
+      .then(e => window.changeScreen('thanks'));
+    return true;
   }
-  nextQuestion(next)
+  nextQuestion(next);
 }
 
 // toggle homescreen
 window.changeScreen = action => {
-  document.getElementById('welcome').classList = 'hide'
-  document.getElementById('questions').classList = 'hide'
-  document.getElementById('thanks').classList = 'hide'
-  document.getElementById(action).classList = 'show'
-  if (action === 'questions') document.querySelector(`#q-1 input[type="text"]`).focus()
+  document.getElementById('welcome').classList = 'hide';
+  document.getElementById('questions').classList = 'hide';
+  document.getElementById('thanks').classList = 'hide';
+  document.getElementById(action).classList = 'show';
+  if (action === 'questions') {
+    document.querySelector('#q-1 input[type="text"]').focus();
+  }
 }
